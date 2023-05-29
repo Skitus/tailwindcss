7353ad96d5c1ed8a736288713a3e16a80375f3d2
Artur Demenskiy <artur.demenskiy@elrom.org>
2023-05-29 22:56:12 +0300

    feat: create init project

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      lg: {
+        max: "992px",
+      },
+      md: {
+        max: "768px",
+      },
+      sm: {
+        max: "480px",
+      },
+    },
+    container: {
+      padding: "20px",
+      center: true,
+    },
+    extend: {
+      colors: {
+        lightBlack: "#4D4244",
+        lightRed: "#FF0D38",
+        darkRed: "#D70026",
+        lightGray: "#747474",
+        darkGray: "#272727",
+      },
+      boxShadow: {
+        customRed: "0px 0px 30px rgba(255, 13, 56, 0.21)",
+      },
+    },
+  },
+  plugins: [],
+};
